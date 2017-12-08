5562165dc54a46a2553348376b47aa4ef94bfeb6
Kelvin <kelvin.roche@upr.edu>
2017-12-08 14:43:16 -0400

    Finished Mat's Class Structure.

--- src/approaches/MatApproach.java
+++ src/approaches/MatApproach.java
@@ -24,4 +24,48 @@ public class MatApproach implements Approach
 
 	}
 
+    public float getProfit() {
+        return profit;
+    }
+
+    public void setProfit(float profit) {
+        this.profit = profit;
+    }
+
+    public int getDisCustomers() {
+        return disCustomers;
+    }
+
+    public void setDisCustomers(int disCustomers) {
+        this.disCustomers = disCustomers;
+    }
+
+    public Queue<Customer> getInputQueue() {
+        return inputQueue;
+    }
+
+    public void setInputQueue(Queue<Customer> inputQueue) {
+        this.inputQueue = inputQueue;
+    }
+
+    public Stack<Customer> getProcessingStack() {
+        return processingStack;
+    }
+
+    public void setProcessingStack(Stack<Customer> processingStack) {
+        this.processingStack = processingStack;
+    }
+
+    public ArrayList<Customer> getServedCustomers() {
+        return servedCustomers;
+    }
+
+    public void setServedCustomers(ArrayList<Customer> servedCustomers) {
+        this.servedCustomers = servedCustomers;
+    }
+
+    public String toString()
+    {
+        return "\nMat's Approach Profit: $" + profit + "\nMat's approach number of dissapointed customers: " + disCustomers;
+    }
 }
